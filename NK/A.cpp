7117593343739efdb081d07cb51a76edb0c571b0
admid <2365203321@qq.com>
2022-11-06 20:35:08 +0800

    no

--- NK/A.cpp
+++ NK/A.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
 using namespace std;
-int main(){
-    return 0;
+int  main(){
+    long long n,m,t,i,ans;
+    cin >> t;
+    while (t--){
+        scanf("%lld %lld",&n,&m);
+        if (m <= n - m)printf("%lld\n",m*4);
+        else{
+            i = n- m ;
+            ans = i * 4 ;
+            m -= i;
+            ans += ((m +1)/2) * 4;
+            printf("%lld\n",ans);
+        }
+    }
+    return  0 ;
 }
